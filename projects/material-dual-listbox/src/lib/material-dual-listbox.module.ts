6f3c6f3ec8a557bf59ff9d218e2c5f473a50aab7
Vlad <vlad.rck@gmail.com>
2020-07-26 15:23:56 +0300

    cleaned

--- projects/material-dual-listbox/src/lib/material-dual-listbox.module.ts
+++ projects/material-dual-listbox/src/lib/material-dual-listbox.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MaterialDualListboxComponent } from './material-dual-listbox.component';
-import {MatInputModule} from '@angular/material/input'
-import {MatIconModule} from '@angular/material/icon';
-import {MatListModule} from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input'
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import {MatFormFieldModule} from '@angular/material/form-field'
+import { MatFormFieldModule } from '@angular/material/form-field'
 import { DragDropModule } from '@angular/cdk/drag-drop';
 @NgModule({
   declarations: [MaterialDualListboxComponent],
